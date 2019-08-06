2228b9604df459cceef9b745fde613f32f738c93
Jose Collas <joseherminiocollas@goatstonc.com>
2019-08-06 09:19:34 -0700

    seperate out component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { hierarchyData } from './hierarchy'
 import HTree from './hierarchy-tree'
+import HierTree from './components/HierTree'
 
 const hTree = HTree(hierarchyData)
 
 function App() {
   const [trigger, setTrigger] = useState(0)
-  const offset = [10, 10]
   useEffect((e) => {
     hTree.setTree()
     setTrigger(1)
@@ -14,53 +14,8 @@ function App() {
 
   return (
     <div className="App">
+      <HierTree hTree={hTree} />
       {trigger}
-      <svg
-        width="340"
-        height="340"
-        style={{ position: 'absolute', top: '40px' }}>
-        <rect x="0" y="0" width="340" height="340" fill="lightgray" />
-        <g
-          transform={`translate(${offset[0]} ${offset[1]})`}
-        >
-          <rect
-            fill="lightgreen"
-            x="0"
-            y="0"
-            width="320"
-            height="320" />
-          <g
-            id="chart-group"
-            transform={`translate(${offset[0]} ${50})`}
-          >
-            {hTree.getTree() && hTree.getTree().descendants()
-              .map(e => (
-                <g
-                  transform={`translate(${e.x} ${e.y})`}
-                  width="100"
-                  height="200">
-                  <text x={12} y={7} fill="red">{e.data.name}</text>
-                  <circle fill="darkblue" cx={0} cy={0} r="5" />
-                </g>
-              ))
-            }
-            {hTree.getTree() && hTree.getTree().links()
-              .map(e => (
-                <g>
-                  <line
-                    fill="blue"
-                    stroke="darkgreen"
-                    x1={e.source.x}
-                    y1={e.source.y}
-                    x2={e.target.x}
-                    y2={e.target.y}
-                  />
-                </g>
-              ))
-            }
-          </g>
-        </g>
-      </svg>
     </div>
   );
 }
---
+++ src/components/HierTree.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const HierTree = ({ hTree }) => {
+  const offset = [10, 10]
+  return (
+    <svg
+      width="340"
+      height="340"
+      style={{ position: 'absolute', top: '40px' }}>
+      <rect x="0" y="0" width="340" height="340" fill="lightgray" />
+      <g
+        transform={`translate(${offset[0]} ${offset[1]})`}
+      >
+        <rect
+          fill="lightgreen"
+          x="0"
+          y="0"
+          width="320"
+          height="320" />
+        <g
+          id="chart-group"
+          transform={`translate(${offset[0]} ${50})`}
+        >
+          {hTree.getTree() && hTree.getTree().descendants()
+            .map(e => (
+              <g
+                transform={`translate(${e.x} ${e.y})`}
+                width="100"
+                height="200">
+                <text x={12} y={7} fill="red">{e.data.name}</text>
+                <circle fill="darkblue" cx={0} cy={0} r="5" />
+              </g>
+            ))
+          }
+          {hTree.getTree() && hTree.getTree().links()
+            .map(e => (
+              <g>
+                <line
+                  fill="blue"
+                  stroke="darkgreen"
+                  x1={e.source.x}
+                  y1={e.source.y}
+                  x2={e.target.x}
+                  y2={e.target.y}
+                />
+              </g>
+            ))
+          }
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export default HierTree
